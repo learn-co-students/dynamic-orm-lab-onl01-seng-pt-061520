a2aea9068ec15cbd711581bdc0545666cdd6a479
Cameron Hudson <camnn3@yahoo.com>
2020-09-05 15:01:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -37,7 +37,7 @@ end
 
 def values_for_insert
   values = []
-  self.class.column_names.eac do |col_name|
+  self.class.column_names.each do |col_name|
   values << "'#{send(col_name)}'" unless send(col_name).nil?
   end
   values.join(", ")
