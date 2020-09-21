c1f38a8be3c7e3a7155ec2f55eeda11574b573f0
idalispaz <idalispaz@gmail.com>
2020-09-21 16:59:07 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,10 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+    #will inherite from 'InteractiveRecord' class
+    #Your methods won't be writting here, all the mehtods defined in 'InteravtiveRecord' 
+    #will be available to 'student' once you set up the inheritence
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+    end 
 end
